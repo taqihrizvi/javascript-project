9bae865d81d8185fa054c50fedcbf5cfd86cb4f2
Farwa <syedafarwa1218@gmail.com>
2024-06-23 22:42:39 +0500

    first challenge completed

---
+++ starter/script.js
@@ -0,0 +1,38 @@
+
+// let js="amazing"
+// if(js==="amazing") alert("lets begin")
+// console.log(23-89-32+190)
+
+//variable and values
+
+
+
+// let country = "Pakistan"
+// let continent= "Asia"
+// let population=260000000
+
+// defined and undefined datatypes
+
+
+// let isIsland= false
+// let language;
+// console.log(typeof country)
+// console.log(typeof continent)
+// console.log(typeof population)
+// console.log(typeof isIsland)
+// console.log(typeof language)
+
+
+
+//coding challenge#1
+
+// let markWeight=78;
+// let markHeight=1.69;
+// let johnWeight=92;
+// let johnHeight=1.95;
+//  //BMI = mass / height ** 2 = mass / (height * height)
+//  markBMI=markWeight/markHeight ** 2;
+//  console.log(markBMI);
+//  johnBMI=johnWeight/johnHeight ** 2;
+//  console.log(johnBMI);
+//  console.log(markBMI>johnBMI)
